1cf0c3bd9cade8f45aff0851f05e950b885cbf0b
beta-tank <beta-tank@outlook.com>
2015-04-12 18:36:48 +0600

    Init Solution.

---
+++ Nola/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nola.Startup))]
+namespace Nola
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
