18523ac13a7604863c472d50059b96101e59cccd
beta-tank <beta-tank@outlook.com>
2015-04-13 16:39:12 +0600

    Add some user models. Add roles DbContext.

--- Nola/Models/Identity/IdentityModels.cs
+++ Nola/Models/Identity/IdentityModels.cs
@@ -3,12 +3,16 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
 namespace Nola.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public IUserInfo UserInfo { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,6 +20,11 @@ namespace Nola.Models
             // Add custom user claims here
             return userIdentity;
         }
+    }
+
+    public class ApplicationRole : IdentityRole
+    {
+        public string DisplayName { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -28,6 +37,19 @@ namespace Nola.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+    }
+
+    class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, string> store)
+            : base(store)
+        { }
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options,
+                                                IOwinContext context)
+        {
+            return new ApplicationRoleManager(new
+                    RoleStore<ApplicationRole>(context.Get<ApplicationDbContext>()));
         }
     }
 }
