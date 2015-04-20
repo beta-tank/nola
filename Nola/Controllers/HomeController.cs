d0b83d9a6db3abcb8e95e4048e3125413da26020
beta-tank <beta-tank@outlook.com>
2015-04-20 22:59:22 +0600

    Create mappings for ApplicationClaim.

--- Nola/Controllers/HomeController.cs
+++ Nola/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using Nola.Models;
 
 namespace Nola.Controllers
 {
---
+++ Nola/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace Nola.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(m =>
+            {
+                m.AddProfile<DomainToModelsMappings>();
+            });
+        } 
+    }
+}
