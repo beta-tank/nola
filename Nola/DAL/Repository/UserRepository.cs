0dac436f8e86b84413e4cb171d889fa587e96f06
beta-tank <beta-tank@outlook.com>
2015-04-28 17:39:50 +0600

    Rename UserRepository.

---
+++ Nola/DAL/Repository/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Nola.Core;
+
+namespace Nola.DAL.Repository
+{
+    public class UserRepository : RepositoryBase<ApplicationUser>, IUserRepository
+    {
+        public UserRepository(IApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    public interface IUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
