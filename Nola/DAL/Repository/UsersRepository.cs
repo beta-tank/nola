69bb66c144693f2f0d8bb5b642d7ecf4577f47a7
beta-tank <beta-tank@outlook.com>
2015-04-28 17:35:55 +0600

    Add UsersRepository.

---
+++ Nola/DAL/Repository/UsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Nola.Core;
+
+namespace Nola.DAL.Repository
+{
+    public class UsersRepository : RepositoryBase<ApplicationUser>, IUsersRepository
+    {
+        public UsersRepository(IApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    public interface IUsersRepository : IRepository<ApplicationUser>
+    {
+    }
+}
