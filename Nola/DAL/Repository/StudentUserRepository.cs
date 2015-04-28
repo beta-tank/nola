5cb7032faf6c39c357bdcb389ffc31de06acc710
beta-tank <beta-tank@outlook.com>
2015-04-28 19:10:27 +0600

    Add Teacher and User repositories.

---
+++ Nola/DAL/Repository/StudentUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Nola.Models;
+
+namespace Nola.DAL.Repository
+{
+    public class StudentUserRepository : BaseInheritanceRepository<BaseUser, StudentUser>, IStudentUserRepository
+    {
+        protected StudentUserRepository(IApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    public interface IStudentUserRepository : IRepository<StudentUser>
+    {
+    }
+}
---
+++ Nola/DAL/Repository/TeacherUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Nola.Models;
+
+namespace Nola.DAL.Repository
+{
+    public class TeacherUserRepository : BaseInheritanceRepository<BaseUser, TeacherUser>, ITeacherUserRepository
+    {
+        protected TeacherUserRepository(IApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public interface ITeacherUserRepository : IRepository<TeacherUser>
+    {
+    }
+}
