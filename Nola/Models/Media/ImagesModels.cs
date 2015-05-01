94737f0509f52de86e09be3ae40cfe13e5b9fcd9
beta-tank <beta-tank@outlook.com>
2015-05-01 23:06:18 +0600

    Edit images models.

--- Nola/Models/Media/ImagesModels.cs
+++ Nola/Models/Media/ImagesModels.cs
@@ -16,9 +16,6 @@ namespace Nola.Models
 
     public class ImageLocal : ImageBase
     {
-        public int Id { get; set; }
-        public string Link { get; set; }
-        public string Description { get; set; }
         public string Name { get; set; }
         public string OriginalName { get; set; }
         public string Path { get; set; }       
@@ -26,9 +23,7 @@ namespace Nola.Models
 
     public class ImageRemote : ImageBase
     {
-        public int Id { get; set; }
-        public string Link { get; set; }
-        public string Description { get; set; }
+
     }
 }
 
