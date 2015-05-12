ee5d5a8a84e1ea5b6f8fa68cb56b1c9bee9bfd7c
beta-tank <beta-tank@outlook.com>
2015-05-12 20:00:35 +0600

    FIx injection config. Small renaming.

--- Nola/App_Start/NinjectConfig.cs
+++ Nola/App_Start/NinjectConfig.cs
@@ -72,10 +72,20 @@ namespace Nola.App_Start
                 .BindDefaultInterface() // Binds the default interface to them;
                 .Configure(b => b.InRequestScope());
 
-                o.FromAssembliesMatching("Nola.Core.dll") // Scans currently assembly
+                o.FromAssembliesMatching("Nola.*.dll") // Select assembly
                 .SelectAllClasses() // Retrieve all non-abstract classes
                 .BindDefaultInterface() // Binds the default interface to them;
                 .Configure(b => b.InRequestScope());
+
+                //o.FromAssembliesMatching("Nola.Data.dll") // Select assembly
+                //.SelectAllClasses() // Retrieve all non-abstract classes
+                //.BindDefaultInterface() // Binds the default interface to them;
+                //.Configure(b => b.InRequestScope());
+
+                //o.FromAssembliesMatching("Nola.Service.dll") // Select assembly
+                //.SelectAllClasses() // Retrieve all non-abstract classes
+                //.BindDefaultInterface() // Binds the default interface to them;
+                //.Configure(b => b.InRequestScope());
             });
         }
     }
--- Nola/Controllers/AccountController.cs
+++ Nola/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace Nola.Controllers
                     return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
-                case SignInStatus.RequiresVerification:
+                case SignInStatus.RequiresVerification: // TODO: проверить отправку кода
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
@@ -149,32 +149,44 @@ namespace Nola.Controllers
 
         //
         // POST: /Account/Register
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Register(RegisterViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+        //        var result = await UserManager.CreateAsync(user, model.Password);
+        //        if (result.Succeeded)
+        //        {
+        //            await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+        //            // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+        //            // Send an email with this link
+        //            // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+        //            // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+        //            // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-            }
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        AddErrors(result);
+        //    }
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+        [AllowAnonymous]
+        public async Task<ActionResult> RegisterStudent()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> RegisterTeacher()
+        {
+            return View();
         }
 
         //
