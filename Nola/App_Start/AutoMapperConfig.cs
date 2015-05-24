acc3d02b79e8c3d85afe6930bf2524c97599eb68
beta-tank <beta-tank@outlook.com>
2015-05-24 13:31:08 +0600

    Reconfigure AutoMapper for correct DI.

--- Nola/App_Start/AutoMapperConfig.cs
+++ Nola/App_Start/AutoMapperConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ninject;
+using Nola.App_Start;
 using Nola.Core.Mappings;
 using Nola.Mappings;
 
@@ -6,16 +8,20 @@ namespace Nola
 {
     public static class AutoMapperBootstrapper
     {
+        //private static IKernel kernel;
         public static void Initialize()
         {
+            //kernel = new StandardKernel(new ServiceModule());  
             Mapper.AssertConfigurationIsValid();
-            Configure();
+            Configure();           
         }
 
         public static void Configure()
         {
+            //Mapper.Configuration.ConstructServicesUsing((type) => NinjectWebCommon.Kernel.Get(type));
             Mapper.Initialize(m =>
             {
+                m.ConstructServicesUsing((type) => NinjectWebCommon.Kernel.Get(type));
                 m.AddProfile<DomainToModelsMappings>();
                 m.AddProfile<ViewModelsToDomainMappings>();
             });
