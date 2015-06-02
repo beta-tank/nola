9cd54a210884cad1e2f64ae5f79ee798b9c3728c
beta-tank <beta-tank@outlook.com>
2015-06-02 15:21:30 +0600

    Add question models.

---
+++ Nola.Core/Models/Question/BaseOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Nola.Core.Models.Question
+{
+    public abstract class BaseOption : IEntity, IOption
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool IsRight { get; set; }
+    }
+}
