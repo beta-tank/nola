98fd2085b6b13e4fe41af69a6dcbebdb9d18e14c
beta-tank <beta-tank@outlook.com>
2015-06-05 00:50:47 +0600

    Add question services.

---
+++ Nola.Service/Services/OptionService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Data.Repository;
+using Nola.Core.Data;
+using Nola.Core.Models.Question;
+
+namespace Nola.Service.Services
+{
+    public interface IOptionService : IService
+    {
+        BaseOption GetOption(int id);
+        void AddOption(BaseOption option);
+        void UpdateOption(BaseOption option);
+        void DeleteOption(BaseOption option);
+        IEnumerable<BaseOption> GetAll();
+    }
+
+    public class OptionService : IOptionService
+    {
+        private readonly IBaseOptionRepository optionRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public OptionService(IBaseOptionRepository optionRepository, IUnitOfWork unitOfWork)
+        {
+            this.optionRepository = optionRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Commit()
+        {
+           unitOfWork.Commit();
+        }
+
+        public BaseOption GetOption(int id)
+        {
+            return optionRepository.GetById(id);
+        }
+
+        public void AddOption(BaseOption option)
+        {
+            optionRepository.Add(option);
+        }
+
+        public void UpdateOption(BaseOption option)
+        {
+            optionRepository.Update(option);
+        }
+
+        public void DeleteOption(BaseOption option)
+        {
+            optionRepository.Delete(option);
+        }
+
+        public IEnumerable<BaseOption> GetAll()
+        {
+            return optionRepository.GetAll();
+        }
+    }
+}
---
+++ Nola.Service/Services/QuestionService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Data.Repository;
+using Nola.Core.Data;
+using Nola.Core.Models.Question;
+
+namespace Nola.Service.Services
+{
+    public interface IQuestionService : IService
+    {
+        BaseQuestion GetQuestion(int id);
+        void AddQuestion(BaseQuestion question);
+        void UpdateQuestion(BaseQuestion question);
+        void DeleteQuestion(BaseQuestion question);
+        IEnumerable<BaseQuestion> GetAll();
+    }
+
+    public class QuestionService : IQuestionService
+    {
+        private readonly IBaseQuestionRepository questionRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public QuestionService(IBaseQuestionRepository questionRepository, IUnitOfWork unitOfWork)
+        {
+            this.questionRepository = questionRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Commit()
+        {
+            unitOfWork.Commit();
+        }
+
+        public BaseQuestion GetQuestion(int id)
+        {
+            return questionRepository.GetById(id);
+        }
+
+        public void AddQuestion(BaseQuestion question)
+        {
+            questionRepository.Add(question);
+        }
+
+        public void UpdateQuestion(BaseQuestion question)
+        {
+            questionRepository.Update(question);
+        }
+
+        public void DeleteQuestion(BaseQuestion question)
+        {
+            questionRepository.Delete(question);
+        }
+
+        public IEnumerable<BaseQuestion> GetAll()
+        {
+            return questionRepository.GetAll();
+        }
+    }
+}
